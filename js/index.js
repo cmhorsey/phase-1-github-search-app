a175f906483e0dfd119abfd1c69dfd91b6de4bcc
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-23 10:39:34 -0400

    Request repo data

--- js/index.js
+++ js/index.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   form.addEventListener('submit', (e) => {
     e.preventDefault()
+    let ul = document.getElementById('user-list')
+    ul.innerHTML = ''
     let query = form.search.value
 
     fetch(`https://api.github.com/search/users?q=${query}`, {
@@ -34,16 +36,52 @@ document.addEventListener('DOMContentLoaded', () => {
           let profilePic = document.createElement('li')
           let profileLink = document.createElement('li')
           let img = document.createElement('img')
+          let btn = document.createElement('button')
+
+          
 
           img.src = item.avatar_url
           displayName.innerText = item.login
-          profileLink.innerText = item.html_url
+          profileLink.innerHTML = `
+          <a href="${item.html_url}">Check me out</a>
+          `
+
+          img.setAttribute('id', 'profile-pic')
 
           profilePic.appendChild(img)
           ul.appendChild(displayName)
           ul.appendChild(profilePic)
           ul.appendChild(profileLink)
-        });
+
+          //add an invent listener to img
+          //on click
+          //get request to https://api.github.com/users/${item.login}/repos
+          //forEach repo
+          //create 
+
+          img.addEventListener('click', (e) => {
+            // console.log(item.login)
+            fetch(`https://api.github.com/users/${item.login}/repos`, {
+              headers:
+                  {
+                  Accept: "application/vnd.github.v3+json"
+                }
+              })
+            .then(res => res.json())
+            .then(repos => {
+              repos.forEach(repo => {
+                console.log(repo.name)
+                console.log(repo.html_url)
+
+              })
+            })
+            
+
+          })
+
+        })
       })
+
+
   })
 })
